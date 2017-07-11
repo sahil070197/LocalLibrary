388a5af44592bf817b02bd01e1853fe2893e7538
palakgarg2107 <sahil.070197@gmail.com>
2017-07-11 16:37:14 +0530

    First user page and basic template, view, models interaction

--- catalog/views.py
+++ catalog/views.py
@@ -4,7 +4,17 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 from django.http import HttpResponse
+
+from .models import Genre, Book, BookInstance, Author
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Bonjour Monde!')
+	num_books=Book.objects.all().count()
+	num_instances=BookInstance.objects.all().count()
+	num_instances_available=BookInstance.objects.filter(status__exact='a').count()
+	num_authors=Author.objects.count() #'all is default'
+	return render(
+		request,
+		'index.html',
+		context={'num_books':num_books, 'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+		)
